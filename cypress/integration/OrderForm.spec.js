8594b09cd9b60cb389fa9a843b57fd6759f5c50f
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-10 10:56:22 -0600

    Begin order form testing with post

---
+++ cypress/integration/OrderForm.spec.js
@@ -0,0 +1,77 @@
+describe ('Main Page View', () => {
+
+    beforeEach(() => {
+              cy.intercept(
+                {
+                    method: "GET",
+                    url: "http://localhost:3001/api/v1/orders"
+                }, 
+                {
+                    status: 201,
+                    body: { 
+                        orders: [
+                        {
+                        "id": 1,
+                        "name": "Lizet",
+                        "ingredients": [
+                        "beans",
+                        "lettuce",
+                        "carnitas",
+                        "queso fresco",
+                        "jalapeno"
+                        ]
+                        } ] 
+                    }
+                })
+                
+            cy.intercept(
+                {
+                    method: 'POST',
+                    url: "http://localhost:3001/api/v1/orders"
+                },
+                {
+                    status: 201,
+                    body: { 
+                        orders: [
+                        {
+                        "id": 1,
+                        "name": "Luly",
+                        "ingredients": [
+                        "frijoles",
+                        "pico de gallo",
+                        "carne asada",
+                        "queso fresco",
+                        "cilantro"
+                        ]
+                        } ] 
+                    }
+                })
+                
+        cy.visit('http://localhost:3000');
+      }
+      
+      );
+    
+    
+    it('Should be able to visit the main page view', () => {
+        cy.visit('http://localhost:3000')
+            .contains('Burrito Builder')
+
+    });
+
+    it('Should be able to see a form upon page load', () => {
+        cy.get('form')
+        cy.get('button').should('have.class', 'ingredientsBtns')
+        cy.get('button').should('have.class', 'submit-btn')
+    });
+
+    it('Should be able to see customer order information ', () => {
+        cy.get('#Lizet').contains("Customer Name: Lizet")
+        cy.get('#ingredientNames').contains('beanslettucecarnitasqueso frescojalapeno')
+    });
+
+    
+
+
+    
+});

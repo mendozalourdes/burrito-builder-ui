1fc283a5854c4cc036b56881ca54bf7a67e0b3c4
lourdesmendoza <lourd.mendoza@gmail.com>
2021-08-10 10:43:08 -0600

    Create cypress  tests for App

---
+++ cypress/integration/App.spec.js
@@ -0,0 +1,43 @@
+describe ('Main Page View', () => {
+
+    beforeEach(() => {
+              cy.intercept(
+                {
+                    method: "GET",
+                    url: "http://localhost:3001/api/v1/orders"
+                }, 
+                {
+                    status: 201,
+                    body: { 
+                        orders: [
+                        {
+                        "id": 1,
+                        "name": "Lizet",
+                        "ingredients": [
+                        "beans",
+                        "lettuce",
+                        "carnitas",
+                        "queso fresco",
+                        "jalapeno"
+                        ]
+                        } ] 
+                    }
+                }) 
+        cy.visit('http://localhost:3000');
+      }
+      
+      );
+    
+    
+    it('Should be able to visit the main page view', () => {
+        cy.visit('http://localhost:3000')
+            .contains('Burrito Builder')
+
+    });
+
+    it('Should be able to see a form upon page load', () => {
+        cy.get('form')
+    });
+
+    
+});
